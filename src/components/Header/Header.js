3d4341facf5776673003dab57f0e554bef70cf92
nurmdrafi <nurmdrafi2014@gmail.com>
2022-04-09 23:57:07 +0600

    implement user info conditional rendering

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,18 +5,23 @@ import useFirebase from '../../hooks/useFirebase';
 const Header = () => {
     const {user, handleSignOut} = useFirebase({});
     return (
-        <div className='d-flex justify-content-center'>
+        <div className='container-fluid d-flex justify-content-center align-items-center my-2' style={{height: '80px'}}>
             <Link to="/" className='text-decoration-none m-3 fw-bold text-black'>Home</Link>
             <Link to="/products" className='text-decoration-none m-3 fw-bold text-black'>Products</Link>
             <Link to="/orders" className='text-decoration-none m-3 fw-bold text-black'>Orders</Link>
+           {/* User Name / User Picture / Log In / Register */}
+            <div className='ms-auto'>
+            <span className='fw-bold text-black'>{user?.displayName && user.displayName}</span>
+            <img src={user?.photoURL} alt="" className='mx-3' style={{width: '50px', borderRadius: '50px'}}/>
             {
                 user?.uid ? <button onClick={handleSignOut} className='btn fw-bold text-danger'>Log Out</button> 
                 : 
-                <div className='my-3'>
-                    <Link to="/login" className='text-decoration-none m-3 fw-bold text-success'>Log In</Link>
-                    <Link to="/register" className='text-decoration-none m-3 fw-bold text-primary'>Register</Link>
-                </div>
+                <span>
+                    <Link to="/login" className='text-decoration-none mx-3 fw-bold text-success'>Log In</Link>
+                    <Link to="/register" className='text-decoration-none mx-3 fw-bold text-primary'>Register</Link>
+                </span>
             }
+            </div>
             
         </div>
     );
